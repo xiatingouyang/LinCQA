87fe9210b240b6c02151a2db2e2f98ebb9167361
Xiating Ouyang <xiating@Xiatings-MacBook-Pro.local>
2021-12-01 03:21:18 -0600

    add run

---
+++ benchmarks/worst/run.sh
@@ -0,0 +1,92 @@
+for algo in fastfo; do
+
+        query=2path
+
+        b=800
+        c=800
+        # for k in 0; do
+        for k in {0..8}; do
+                let a=120+460*k
+                let N=1000000+500000*k
+                let incon=100*a*b+100*b*c
+                let total=2*N
+                let ratio=incon/total
+                echo $a $b $c $N $ratio
+
+                sqlcmd -S localhost -U sa -P cqa2021! -i reset-worst.sql
+                ./gen $a $b $N > data/r_1.csv
+                ./gen $b $c $N > data/r_2.csv
+                ./gen 2 2 100 > data/r_3.csv
+                python3 bulk-populate.py
+
+                for it in {1..3}; do
+                        { time sqlcmd -S localhost -U sa -P cqa2021! -d worst -i ${query}-${algo}.sql > output ; } 2>> results/${algo}_${a}_${b}_${c}.txt
+                done
+
+        done
+
+        N=1000000
+        # for a in 100; do
+        for a in {100..1000..90}; do
+                let incon=100*a*b+100*b*c
+                let total=2*N
+                let ratio=incon/total
+                echo $a $b $c $N $ratio
+
+                sqlcmd -S localhost -U sa -P cqa2021! -i reset-worst.sql
+                ./gen $a $b $N > data/r_1.csv
+                ./gen $b $c $N > data/r_2.csv
+                ./gen 2 2 100 > data/r_3.csv
+                python3 bulk-populate.py
+
+                for it in {1..3}; do
+                        { time sqlcmd -S localhost -U sa -P cqa2021! -d worst -i ${query}-${algo}.sql > output ; } 2>> results/${algo}_${a}_${b}_${c}.txt
+                done
+
+        done
+
+
+        query=3path
+        b=120
+        c=120
+        d=120
+        # for k in 0; do
+        for k in {0..8}; do 
+                let a=120+180*k
+                let N=1000000+500000*k
+                let incon=100*a*b+100*b*c+100*c*d
+                let total=3*N
+                let ratio=incon/total
+                echo $a $b $c $d $N $ratio 
+
+                sqlcmd -S localhost -U sa -P cqa2021! -i reset-worst.sql
+                ./gen $a $b $N > data/r_1.csv
+                ./gen $b $c $N > data/r_2.csv
+                ./gen $c $d 100 > data/r_3.csv
+                python3 bulk-populate.py
+
+                for it in {1..3}; do
+                        { time sqlcmd -S localhost -U sa -P cqa2021! -d worst -i ${query}-${algo}.sql > output ; } 2>> results/${algo}_${a}_${b}_${c}_${d}.txt
+                done
+        done
+
+        N=1000000
+        # for a in 200; do
+        for a in {200..8000..600}; do
+                let incon=100*a*b+100*b*c+100*c*d
+                let total=3*N
+                let ratio=incon/total
+                echo $a $b $c $d $N $ratio 
+
+                sqlcmd -S localhost -U sa -P cqa2021! -i reset-worst.sql
+                ./gen $a $b $N > data/r_1.csv
+                ./gen $b $c $N > data/r_2.csv
+                ./gen $c $d 100 > data/r_3.csv
+                python3 bulk-populate.py
+
+                for it in {1..3}; do
+                        { time sqlcmd -S localhost -U sa -P cqa2021! -d worst -i ${query}-${algo}.sql > output ; } 2>> results/${algo}_${a}_${b}_${c}_${d}.txt
+                done
+        done
+done
+
