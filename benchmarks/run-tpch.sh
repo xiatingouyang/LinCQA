92fa10301db6c5be9c32bf41bf5e4959eb38bb4e
Xiating Ouyang <xouyang@cs.wisc.edu>
2022-03-25 11:35:33 +0800

    finishing tpch

--- benchmarks/run-tpch.sh
+++ benchmarks/run-tpch.sh
@@ -4,7 +4,7 @@ for rewriting in sqls conquer lincqa-sqls; do
 	for scale in 1 10; do
 	       for qid in 1 2 3 4 6 10 11 12 14 16 17 18 20 21; do
 	       		for it in {1..3}; do
-                        { time sqlcmd -S localhost -U sa -P cqa2022! -d tpch_${scale} -i tpch/${rewriting}/${qid}.sql > output ; } 2>> results/${scale}_${rewriting}_${qid}.txt
+                        { time sqlcmd -S localhost -U sa -P cqa2022! -d tpch_${scale} -i tpch/${rewriting}/${qid}.sql > output ; } 2>> tpch/results/${scale}_${rewriting}_${qid}.txt
                 done
 	        done
 	done
