ee40cc048b0f86b576aa701bb7d2949af58d36a4
Xiating Ouyang <xouyang@cs.wisc.edu>
2022-03-25 11:09:10 +0800

    edits

--- benchmarks/synthetic/populate.sh
+++ benchmarks/synthetic/populate.sh
@@ -1,3 +1,4 @@
 wget http://pages.cs.wisc.edu/~xouyang/programs/synthetic_data.zip
 unzip synthetic_data.zip
+sqlcmd -S localhost -U sa -P cqa2022! -i create_db.sql
 python3 ../bulk-populate.py schemas.json dbinfo.json
---
+++ benchmarks/tpch/create_db.sql
@@ -0,0 +1,2 @@
+create database tpch_1
+create database tpch_10
