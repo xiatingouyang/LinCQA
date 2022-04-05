fef9b624a7c55f2fd71ded506bf4b41e5d44b790
Xiating Ouyang <xiating@Xiatings-MacBook-Pro.local>
2022-04-04 20:33:46 -0500

    finalized sql queries

--- benchmarks/stackoverflow/sqls/3.sql
+++ benchmarks/stackoverflow/sqls/3.sql
@@ -1,3 +1,3 @@
 SELECT P.Title
 FROM Posts P, PostLinks PL, PostHistory PH
-WHERE P.PostId = PL.PostId AND  PL.PostId = PH.PostId AND PH.PostHistoryTypeId = 5 AND P.AnswerCount >= 50
+WHERE P.Id = PL.PostId AND  PL.PostId = PH.PostId AND PH.PostHistoryTypeId = 5 AND P.AnswerCount >= 50
--- benchmarks/stackoverflow/sqls/5.sql
+++ benchmarks/stackoverflow/sqls/5.sql
@@ -1,3 +1,4 @@
 SELECT U.DisplayName
-FROM Users U, Posts P, PostHistory PH, Votes V, Comments C, Badge B
-WHERE U.UserId = C.UserId AND C.UserId = PH.UserId AND PH.UserId = V.UserId AND U.UserId = P.OwnerUserId AND U.DisplayName = PH.UserDisplayName AND P.Id = PH.PostId AND PH.PostId = C.PostId AND C.PostId = V.PostId AND AND PH.PostHistoryTypeId = 5 AND B.UserId = U.Id AND B.Name = "Nice Answer"
+FROM Users U, Posts P, PostHistory PH, Votes V, Comments C, Badges B
+WHERE U.Id = C.UserId AND C.UserId = PH.UserId AND PH.UserId = V.UserId AND U.Id = P.OwnerUserId AND U.DisplayName = PH.UserDisplayName AND P.Id = PH.PostId AND PH.PostId = C.PostId AND C.PostId = V.PostId AND PH.PostHistoryTypeId = 5 AND B.UserId = U.Id AND B.Name = "Nice Answer"
+
