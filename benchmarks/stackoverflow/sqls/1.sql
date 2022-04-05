21a1ecd12453e2f8eccf4f5aed4f199838c724c2
Xiating Ouyang <xiating@Xiatings-MacBook-Pro.local>
2022-04-05 12:35:07 -0500

    add samples

--- benchmarks/stackoverflow/sqls/1.sql
+++ benchmarks/stackoverflow/sqls/1.sql
@@ -1,4 +1,4 @@
 SELECT U.DisplayName
-FROM Users U, Posts P, Votes V, Comments C
+FROM Users_sample U, Posts_sample P, Votes_sample V, Comments_sample C
 WHERE U.Id = P.OwnerUserId AND U.Id = V.UserId AND U.Id = C.UserId
 AND C.PostId = P.Id and C.PostId = V.PostId
