a022caf032e0b65b9f8f0ee540bb491767836516
Xiating Ouyang <xiating@Xiatings-MacBook-Pro.local>
2022-04-08 00:04:58 -0500

    finalizing scripts

--- benchmarks/run-stackoverflow.sh
+++ benchmarks/run-stackoverflow.sh
@@ -1,6 +1,6 @@
 mkdir stackoverflow/results
 
-for rewriting in sqls lincqa conquer fastfo; do
+for rewriting in sqls lincqa/view lincqa/materialize conquer fastfo; do
         for qid in 2 6 8 9 10; do
                 for it in {1..6}; do
                         { timeout 10m sqlcmd -S localhost -U sa -P cqa2022! -d StackOverflow -i stackoverflow/${rewriting}/${qid}.sql > output ; } 2>> stackoverflow/results/${rewriting}_${qid}.txt
