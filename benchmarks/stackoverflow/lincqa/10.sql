8b668136c936006eb79219c8fa7cd6bd05397bf5
Xiating Ouyang <xiating@Xiatings-MacBook-Pro.local>
2022-04-08 09:36:39 -0500

    edit 10

--- benchmarks/stackoverflow/lincqa/10.sql
+++ benchmarks/stackoverflow/lincqa/10.sql
@@ -1,34 +1,34 @@
-drop table candidates
+drop table candidates;
 
-drop table Users_bad_key
-drop table Users_good_join
+drop table Users_bad_key;
+drop table Users_good_join;
 
-drop table Posts_bad_key
-drop table Posts_good_join
+drop table Posts_bad_key;
+drop table Posts_good_join;
 
-drop table PostHistory_bad_key
-drop table PostHistory_good_join
+drop table PostHistory_bad_key;
+drop table PostHistory_good_join;
 
-drop table Comments_bad_key
-drop table Comments_good_join
+drop table Comments_bad_key;
+drop table Comments_good_join;
 
-drop table Badges_bad_key
-drop table Badges_good_join
+drop table Badges_bad_key;
+drop table Badges_good_join;
 
-drop table Votes_bad_key
-drop table Votes_good_join
+drop table Votes_bad_key;
+drop table Votes_good_join;
 
 
-SELECT DISTINCT P.id, P.title, V.PostId, V.UserId, V.CreationDate
+SELECT DISTINCT P.Id, P.title, V.PostId, V.UserId, V.CreationDate
 into candidates 
 FROM Posts P, Votes V
 WHERE P.Id = V.PostId AND P.OwnerUserId = V.UserId AND BountyAmount > 100;
-
+GO
 
 select V.PostId, V.UserId, V.CreationDate 
 into Votes_bad_key
 from Votes V
-join candidates C on (V.PostId = C.PostId and V.UserId = C.UserId and V.CreationDate = C.CreationDate)
+join candidates on (V.PostId = candidates.PostId and V.UserId = candidates.UserId and V.CreationDate = candidates.CreationDate)
 where V.BountyAmount <= 100;
 
 
@@ -38,7 +38,7 @@ where V.BountyAmount <= 100;
 select V.PostId, V.UserId
 into Votes_good_join
 from Votes V
-join candidates C on V.PostId = C.PostId and V.UserId = C.UserId and V.CreationDate = C.CreationDate
+join candidates on V.PostId = candidates.PostId and V.UserId = candidates.UserId and V.CreationDate = candidates.CreationDate
 where not exists (
         select *
         from Votes_bad_key
@@ -63,7 +63,7 @@ with Posts_bad_key as (
 
        select P.Id 
        from Posts P
-       join candidates C on P.Id = C.Id and P.Title = C.Title
+       join candidates on P.Id = candidates.Id and P.Title = candidates.Title
        left outer join Votes_good_join on (P.OwnerUserId = Votes_good_join.UserId)
        where Votes_good_join.UserId is null
 )
@@ -75,7 +75,7 @@ from Posts_bad_key;
 select P.Id, P.Title
 into Posts_good_join
 from Posts P
-join candidates C on P.Id = C.Id and P.Title = C.Title
+join candidates on P.Id = candidates.Id and P.Title = candidates.Title
 where not exists (
     select * 
     from Posts_bad_key
