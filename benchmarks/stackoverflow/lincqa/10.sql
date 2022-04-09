e69c70a885173ffa2b732eca780453cb6a5c13ea
Xiating Ouyang <xiating@Xiatings-MacBook-Pro.local>
2022-04-09 14:30:40 -0500

    add q10 lincqa

--- benchmarks/stackoverflow/lincqa/10.sql
+++ benchmarks/stackoverflow/lincqa/10.sql
@@ -1,91 +1,45 @@
-drop table candidates;
-
-drop table Users_bad_key;
-drop table Users_good_join;
-
-drop table Posts_bad_key;
-drop table Posts_good_join;
-
-drop table PostHistory_bad_key;
-drop table PostHistory_good_join;
-
-drop table Comments_bad_key;
-drop table Comments_good_join;
-
-drop table Badges_bad_key;
-drop table Badges_good_join;
-
-drop table Votes_bad_key;
-drop table Votes_good_join;
+WITH Candidates_Posts AS (
+  SELECT P.id, P.title, V.CreationDate
+  FROM Posts P, Votes V
+  WHERE P.Id = V.PostId AND P.OwnerUserId = V.UserId AND BountyAmount > 100
+),
 
 
-SELECT DISTINCT P.Id, P.title, V.PostId, V.UserId, V.CreationDate
-into candidates 
-FROM Posts P, Votes V
-WHERE P.Id = V.PostId AND P.OwnerUserId = V.UserId AND BountyAmount > 100;
-GO
+Filter_Posts AS (
+  SELECT
+    C.Id
+  FROM
+    Candidates_Posts C
+    JOIN Posts ON C.Id = Posts.Id 
+    LEFT OUTER JOIN Votes V ON C.Id = V.PostId AND Posts.OwnerUserId = V.UserId AND C.CreationDate = V.CreationDate
+  WHERE
+    (V.UserId IS NULL OR V.CreationDate IS NULL
+  or V.BountyAmount <= 100
+  )
+   union all
 
-select V.PostId, V.UserId, V.CreationDate 
-into Votes_bad_key
-from Votes V
-join candidates on (V.PostId = candidates.PostId and V.UserId = candidates.UserId and V.CreationDate = candidates.CreationDate)
-where V.BountyAmount <= 100;
+  select Id
+  from (
+  select distinct Id, Title
+  from Candidates_posts
+ ) t
+  group by Id
+  having count(*) > 1 
 
-
-
-
-
-select V.PostId, V.UserId
-into Votes_good_join
-from Votes V
-join candidates on V.PostId = candidates.PostId and V.UserId = candidates.UserId and V.CreationDate = candidates.CreationDate
-where not exists (
-        select *
-        from Votes_bad_key
-        where
-        V.PostId = Votes_bad_key.PostId and
-        V.UserId = Votes_bad_key.UserId and
-        V.CreationDate = Votes_bad_key.CreationDate
-);
-
-
-
-with Posts_bad_key as (
-       select Id 
-       from (
-       	    select distinct id, Title
-	          from Posts 
-       ) t
-       group by Id 
-       having count(*) > 1
-
-       union all 
-
-       select P.Id 
-       from Posts P
-       join candidates on P.Id = candidates.Id and P.Title = candidates.Title
-       left outer join Votes_good_join on (P.OwnerUserId = Votes_good_join.UserId)
-       where Votes_good_join.UserId is null
 )
-select * into Posts_bad_key
-from Posts_bad_key;
 
 
+SELECT
+  distinct Id, title
+FROM
+  Candidates_Posts C
+WHERE
+  NOT EXISTS (
+    SELECT
+      *
+    FROM
+      Filter_Posts F
+    WHERE
+      C.Id = F.Id
 
-select P.Id, P.Title
-into Posts_good_join
-from Posts P
-join candidates on P.Id = candidates.Id and P.Title = candidates.Title
-where not exists (
-    select * 
-    from Posts_bad_key
-    where P.Id = Posts_bad_key.Id 
-);
-
-
-
-select distinct Posts_good_join.Id, Posts_good_join.title 
-from Votes_good_join, Posts_good_join
-where Posts_good_join.Id = Votes_good_join.PostId
-
-
+)
