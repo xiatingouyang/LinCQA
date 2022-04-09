fd7bfebbd81dc0a36b8399885c5dfc4cd8e99aa7
Xiating Ouyang <xiating@Xiatings-MacBook-Pro.local>
2022-04-09 14:51:22 -0500

    revised 6

--- benchmarks/stackoverflow/lincqa/6.sql
+++ benchmarks/stackoverflow/lincqa/6.sql
@@ -1,13 +1,37 @@
-with candidates as (
-	Select DISTINCT C.UserId, C.CreationDate, P.Id, P.Title
-	FROM Posts P, PostHistory PH, Votes V, Comments C 
-	WHERE P.Tags LIKE "%SQL%" AND P.id = PH.PostId AND P.id = C.PostId AND P.id = V.PostId AND V.BountyAmount > 100 AND PH.PostHistoryTypeId = 2 AND C.score = 0
-),
+drop table candidate
+drop table candidates
 
--- Posts_bad_key as (
+drop table Users_bad_key
+drop table Users_good_join
+
+drop table Posts_bad_key
+drop table Posts_good_join
+
+drop table PostHistory_bad_key
+drop table PostHistory_good_join
+
+drop table Comments_bad_key
+drop table Comments_good_join
+
+drop table Badges_bad_key
+drop table Badges_good_join
+
+drop table Votes_bad_key
+drop table Votes_good_Join
+
+drop table PostHistory_bad_key
+drop table PostHistory_good_join
+
+Select DISTINCT C.UserId, C.CreationDate, P.Id, P.Title
+into candidates
+FROM Posts P, PostHistory PH, Votes V, Comments C 
+WHERE P.Tags LIKE "%SQL%" AND P.id = PH.PostId AND P.id = C.PostId AND P.id = V.PostId AND V.BountyAmount > 100 AND PH.PostHistoryTypeId = 2 AND C.score = 0;
+
+
+-- with Posts_bad_key as (
 -- 	select P.Id 
 -- 	from Posts P
--- 	where P.Tags not LIKE "%SQL%"
+-- 	where P.Tags not LIKE "%SQL%" or P.Tags is null
 
 -- 	union all  
 
@@ -18,59 +42,77 @@ with candidates as (
 -- 	)t 
 -- 	group by Id 
 -- 	having count(*) > 1
--- ),
+-- )
 
-Posts_good_join as (
-	select P.Id, P.Title 
-	from Posts P 
-	join candidates on (P.Id = candidates.Id)
-	where P.Tags LIKE "%SQL%"
-	-- where not exists (
-	-- 	select *
-	-- 	from Posts_bad_key
-	-- 	where P.Id = Posts_bad_key.Id
-	-- )
-),
+-- select *
+-- into Posts_bad_key
+-- from Posts_bad_key;
 
-PostHistory_bad_key as (
-	select PH.PostId, PH.CreationDate, PH.UserId, PH.PostHistoryTypeId 
-	from PostHistory PH
-	where PH.PostHistoryTypeId <> 2 
-),
 
-PostHistory_good_join as (
-	select PH.PostId 
-	from PostHistory PH 
-	where not exists (
-		select *
-		from PostHistory_bad_key
-		where PH.PostId = PostHistory_bad_key.PostId and 
-			PH.CreationDate = PostHistory_bad_key.CreationDate and  
-			PH.UserId = PostHistory_bad_key.UserId and  
-			PH.PostHistoryTypeId = PostHistory_bad_key.PostHistoryTypeId
-	)
-),
+select P.Id, P.Title
+into Posts_good_join
+from Posts P
+join candidates on (P.Id = candidates.Id)
+where P.Tags LIKE "%SQL%"
 
-Votes_bad_key as (
-	select V.PostId, V.UserId, V.CreationDate
-	from Votes V 
-	where V.BountyAmount <= 100
-), 
 
-Votes_good_join as (
-	select V.PostId 
-	from Votes V 
-	where not exists (
-		select *
-		from Votes_bad_key
-		where
-		V.PostId = Votes_bad_key.PostId and  
-		V.UserId = Votes_bad_key.UserId and  
-		V.CreationDate = Votes_bad_key.CreationDate
-	)
-),
+-- select P.Id, P.Title
+-- into Posts_good_join 
+-- from Posts P 
+-- join candidates on (P.Id = candidates.Id)
+-- where not exists (
+-- 	select *
+-- 	from Posts_bad_key
+-- 	where P.Id = Posts_bad_key.Id
+-- );
 
-Comments_bad_key as (
+
+
+select PH.PostId, PH.CreationDate, PH.UserId, PH.PostHistoryTypeId 
+into PostHistory_bad_key
+from PostHistory PH
+where PH.PostHistoryTypeId <> 2;
+
+
+
+
+select PH.PostId 
+into PostHistory_good_join
+from PostHistory PH 
+where not exists (
+	select *
+	from PostHistory_bad_key
+	where PH.PostId = PostHistory_bad_key.PostId and 
+		PH.CreationDate = PostHistory_bad_key.CreationDate and  
+		PH.UserId = PostHistory_bad_key.UserId and  
+		PH.PostHistoryTypeId = PostHistory_bad_key.PostHistoryTypeId
+)
+
+
+
+select V.PostId, V.UserId, V.CreationDate
+into Votes_bad_key
+from Votes V 
+where V.BountyAmount <= 100 or V.BountyAmount is null;
+
+
+
+
+
+select V.PostId 
+into Votes_good_join
+from Votes V 
+where not exists (
+	select *
+	from Votes_bad_key
+	where
+	V.PostId = Votes_bad_key.PostId and  
+	V.UserId = Votes_bad_key.UserId and  
+	V.CreationDate = Votes_bad_key.CreationDate
+);
+
+
+with Comments_bad_key as (
 	select C.CreationDate, C.UserId, candidates.Title	
 	from Comments C 
 	join candidates on (C.CreationDate = candidates.CreationDate and C.UserId = candidates.UserId)
@@ -89,20 +131,26 @@ Comments_bad_key as (
 		Posts_good_join.Id is NULL or PostHistory_good_join.PostId is NULL or Votes_good_join.PostId is NULL
 		or Posts_good_join.Title is NULL
 	)
-),
-
-Comments_good_join as (
-	select candidates.Title	
-	from Comments C 
-	join candidates on (C.CreationDate = candidates.CreationDate and C.UserId = candidates.UserId)
-	where not exists (
-		select * 
-		from Comments_bad_key
-		where C.CreationDate = Comments_bad_key.CreationDate
-		and C.UserId = Comments_bad_key.UserId
-		and candidates.Title = Comments_bad_key.Title
-	)
 )
+
+select *
+into Comments_bad_key
+from Comments_bad_key;
+
+
+
+select candidates.Title
+into Comments_good_join	
+from Comments C 
+join candidates on (C.CreationDate = candidates.CreationDate and C.UserId = candidates.UserId)
+where not exists (
+	select * 
+	from Comments_bad_key
+	where C.CreationDate = Comments_bad_key.CreationDate
+	and C.UserId = Comments_bad_key.UserId
+	and candidates.Title = Comments_bad_key.Title
+);
+
 
 select distinct Title
 from Comments_good_join
