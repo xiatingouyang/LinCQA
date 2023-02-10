67bfa54c2074b2b1bf5b5499aa0d7e71b2964c66
Xiating Ouyang <xouyang@cs.wisc.edu>
2023-02-11 01:13:11 +0800

    bash update

--- run.sh
+++ run.sh
@@ -1,8 +1,26 @@
 
-python3 rewriter.py -s benchmarks/tpch/schemas.json -i benchmarks/tpch/sqls/10.sql -algo lincqa
+# python3 rewriter.py -s benchmarks/tpch/schemas.json -i benchmarks/tpch/sqls/10.sql -algo lincqa
 		
-# for benchmark in tpch; do
-# 	for qid in 1; do
+# for benchmark in synthetic; do
+# 	for qid in {15..21}; do
+# 		for algo in fastfo conquer lincqa; do
+# 			python3 rewriter.py -s benchmarks/${benchmark}/schemas.json -i benchmarks/${benchmark}/sqls/${qid}.sql -algo ${algo}
+# 		done
+# 	done
+# done
+
+
+for benchmark in tpch; do
+	for qid in 1 2 3 4 6 10 11 12 14 16 17 18 20 21; do
+		for algo in fastfo conquer lincqa; do
+			python3 rewriter.py -s benchmarks/${benchmark}/schemas.json -i benchmarks/${benchmark}/sqls/${qid}.sql -algo ${algo}
+		done
+	done
+done
+
+
+# for benchmark in stackoverflow; do
+# 	for qid in {1..5}; do
 # 		for algo in fastfo conquer lincqa; do
 # 			python3 rewriter.py -s benchmarks/${benchmark}/schemas.json -i benchmarks/${benchmark}/sqls/${qid}.sql -algo ${algo}
 # 		done
